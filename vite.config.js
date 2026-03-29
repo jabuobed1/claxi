a1968f6f86d38dbc3812e7543d81717286d33496
jabuobed1 <jabuobed1@gmail.com>
2026-03-29 08:27:20 +0000

    Commit before pull - 4.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/verify-paystack': {
+        target: 'http://localhost:5001/claxi-bakayise/us-central1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/verify-paystack/, '/verifyPaystack'),
+      },
+    },
+  },
 })
