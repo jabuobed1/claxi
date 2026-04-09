faebdcdd46d721ef11ef2f71752f889e8bb4e056
jabuobed1 <jabuobed1@gmail.com>
2026-04-09 11:00:38 +0000

    Commit after hard reset - 09.04.2026 - 7.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1200,
+  },
   server: {
     proxy: {
       '/ice-config': {
